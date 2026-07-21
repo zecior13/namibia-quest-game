bb2951ec89e915a521c65052ad6e35a49553e7de
zecior13 <mioduchowski.radomil@gmail.com>
2026-07-21 21:17:11 +0200

    Use a real alpha vehicle sprite

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -7,7 +7,7 @@ export class BootScene extends Phaser.Scene {
     this.load.image("titleMap", "assets/art/title-map-style-a.png");
     this.load.image("startScene", "assets/art/start-scene-namibia.png");
     this.load.image("startPlate", "assets/art/start-scene-plate.png");
-    this.load.image("startVehicle", "assets/art/start-scene-vehicle.png");
+    this.load.image("startVehicleAlpha", "assets/art/start-scene-vehicle-alpha.png");
     this.load.image("heroDriver", "assets/art/hero-driver-kapitan-4x4.png");
     this.load.image("cargoScene", "assets/pack/cargo-scene.png");
     this.load.image("packItems", "assets/pack/pack-items.png");
@@ -24,66 +24,8 @@ export class BootScene extends Phaser.Scene {
   }
 
   create(){
-    this.prepareTransparentVehicle();
     this.prepareBirdTexture();
     this.scene.start("StartScene");
-  }
-
-  prepareTransparentVehicle(){
-    const source = this.textures.get("startVehicle").getSourceImage();
-    const canvas = document.createElement("canvas");
-    canvas.width = source.width;
-    canvas.height = source.height;
-    const context = canvas.getContext("2d", { willReadFrequently: true });
-    context.drawImage(source, 0, 0);
-
-    const image = context.getImageData(0, 0, canvas.width, canvas.height);
-    const pixels = image.data;
-    const visited = new Uint8Array(canvas.width * canvas.height);
-    const queue = [];
-    const indexFor = (x, y) => y * canvas.width + x;
-    const isBackdrop = (x, y) => {
-      const offset = indexFor(x, y) * 4;
-      const red = pixels[offset];
-      const green = pixels[offset + 1];
-      const blue = pixels[offset + 2];
-      return red > 165 && green > 165 && blue > 165 && Math.max(red, green, blue) - Math.min(red, green, blue) < 35;
-    };
-
-    for(let x = 0; x < canvas.width; x++){
-      queue.push([x, 0], [x, canvas.height - 1]);
-    }
-    for(let y = 1; y < canvas.height - 1; y++){
-      queue.push([0, y], [canvas.width - 1, y]);
-    }
-
-    while(queue.length){
-      const [x, y] = queue.pop();
-      if(x < 0 || y < 0 || x >= canvas.width || y >= canvas.height){
-        continue;
-      }
-      const position = indexFor(x, y);
-      if(visited[position] || !isBackdrop(x, y)){
-        continue;
-      }
-      visited[position] = 1;
-      pixels[position * 4 + 3] = 0;
-      queue.push([x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]);
-    }
-
-    // The checkerboard also forms enclosed pockets beneath the vehicle.
-    // Remove only very bright neutral pixels so the illustrated paint and bags remain intact.
-    for(let offset = 0; offset < pixels.length; offset += 4){
-      const red = pixels[offset];
-      const green = pixels[offset + 1];
-      const blue = pixels[offset + 2];
-      if(red > 165 && green > 165 && blue > 165 && Math.max(red, green, blue) - Math.min(red, green, blue) < 45){
-        pixels[offset + 3] = 0;
-      }
-    }
-
-    context.putImageData(image, 0, 0);
-    this.textures.addCanvas("startVehicleAlpha", canvas);
   }
 
   prepareBirdTexture(){
